a42a873309a55bb7eb5f00ceb1123dba6f2b091e
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-11 09:48:42 -0300

    feat: adicionar aba de configuração à navbar inferior

--- src/pages/Main/Tabs/index.tsx
+++ src/pages/Main/Tabs/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../Home';
+import Settings from '../Settings';
 
 import { Entypo } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -25,6 +27,15 @@ export default function Tabs() {
 				options={{
 					tabBarIcon: ({ color, size }) => (
 						<FontAwesome5 name='user-alt' size={24} color='black' />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Configurações'
+				component={Settings}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Feather name='settings' size={24} color='black' />
 					),
 				}}
 			/>
