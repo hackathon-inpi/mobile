b9d7c64f3239470e39abb664105ffc8b075136a5
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-10 23:53:15 -0300

    feat: apontar arquivo de rotas para Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from './style';
+
+export default function Home() {
+	return <Container />;
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Home from './pages/Home';
 
 export default function routes() {
-	return (
-		<View>
-			<Text>Routes</Text>
-		</View>
-	);
+	return <Home />;
 }
