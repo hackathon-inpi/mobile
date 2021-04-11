7daeedfb3d882bb3f9b25c375a9756cb018cc451
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-11 10:07:51 -0300

    feat: adicionar boilerplate da tela de Perfil

---
+++ src/pages/Main/Profile/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, LogoutButton, LogoutButtonText } from './style';
+
+export default function Home() {
+	return (
+		<Container>
+			<LogoutButtonText>Perfil</LogoutButtonText>
+		</Container>
+	);
+}
---
+++ src/pages/Main/Profile/style.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	background-color: #fac1c1;
+`;
+
+export const LogoutButton = styled.TouchableOpacity`
+	border: 1px solid black;
+	padding: 20px;
+`;
+
+export const LogoutButtonText = styled.Text``;
