8b231ea428ba124cfae6b44c2313d0e48156d062
Ezequiel Demetras <ezequiel-d.s@hotmail.com>
2021-04-11 20:46:15 -0300

    feat: iniciar desenvolvimento main page

--- src/pages/Main/Home/index.tsx
+++ src/pages/Main/Home/index.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
-import { Container, LogoutButtonText } from './style';
+import {
+	Container,
+	LogoutButtonText,
+	Header,
+	Body,
+	HeaderText,
+	ImgProfile,
+	TextWelcome,
+	BoardInfo,
+	BoardText,
+	Div,
+	BoardGraph,
+	BoardFlex,
+	BoardHeaderText,
+	BoardFooterText,
+	RegisterDemand,
+	RegisterText,
+	MainDisplayButtons,
+	MainButtom,
+	ButtomText
+} from './style';
 
 export default function Home() {
 	return (
-		<Container>
-			<LogoutButtonText>Home</LogoutButtonText>
-		</Container>
+		<Body>
+			<Header>
+				<HeaderText>INPI</HeaderText>
+			</Header>
+			<Container>
+				<ImgProfile></ImgProfile>
+				<TextWelcome>Bem-vinda, Ana Lúcia</TextWelcome>
+				<BoardInfo>
+					<BoardFlex>
+						<Div>
+							<BoardHeaderText>Visão Geral</BoardHeaderText>
+							<BoardText>1 marca registrada</BoardText>
+							<BoardText>3 patentes registradas</BoardText>
+							<BoardText>2 softwares registrados</BoardText>
+						</Div>
+						<Div>
+							<BoardGraph>
+								<BoardText></BoardText>
+							</BoardGraph>
+						</Div>
+					</BoardFlex>
+					<BoardFooterText>Você possui 3 pedidos em andamento</BoardFooterText>
+				</BoardInfo>
+				<RegisterDemand>
+					<RegisterText>Registre uma nova criação</RegisterText>
+				</RegisterDemand>
+				<MainDisplayButtons>
+					<MainButtom>
+						<ButtomText>{`Pagar\numa GRU`}</ButtomText>
+					</MainButtom>
+					<MainButtom>
+						<ButtomText>{`Gerar\numa GRU`}</ButtomText>
+					</MainButtom>
+					<MainButtom>
+						<ButtomText>{`Acessar o\nBuscaWeb`}</ButtomText>
+					</MainButtom>
+				</MainDisplayButtons>
+			</Container>
+		</Body>
 	);
 }
--- src/pages/Main/Home/style.tsx
+++ src/pages/Main/Home/style.tsx
@@ -1,10 +1,17 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Body = styled.View`
 	flex: 1;
-	justify-content: center;
+	justify-content: flex-start;
 	align-items: center;
-	background-color: #74bcff;
+	background-color: #F2F2F2;
+`;
+
+export const Container = styled.View`
+	width: 100%;
+	padding: 20px 54px;
+	justify-content: flex-start;
+	align-items: center;
 `;
 
 export const LogoutButton = styled.TouchableOpacity`
@@ -12,4 +19,119 @@ export const LogoutButton = styled.TouchableOpacity`
 	padding: 20px;
 `;
 
+export const Div = styled.View``;
+
 export const LogoutButtonText = styled.Text``;
+
+export const Header = styled.View`
+	width: 100%;
+	height: 92px;
+	padding: 20px;
+	background-color: #1351b4;
+	justify-content: center;
+	align-items: flex-start;
+`;
+
+export const HeaderText = styled.Text`
+	margin-top: 20px;
+	font-weight: bold;
+	color: #FFFFFF;
+	font-size: 16px;
+
+`;
+
+export const ImgProfile = styled.View`
+	width: 100px;
+	height: 100px;
+	border-radius: 100px;
+	background-color: #1351b4;
+`;
+
+export const TextWelcome = styled.Text`
+	margin: 20px 0px;
+	font-size: 20px;
+	color: #6698EB;
+`;
+
+export const BoardInfo = styled.View`
+	width: 100%;
+	border-width: 1px;
+	border-color: #1351b4;
+	background-color: #FFFFFF;
+	border-radius: 8px;
+	padding: 24px 12px;
+`;
+
+export const BoardFlex = styled.View`
+	flex-direction: row;
+	justify-content: space-around;
+`;
+
+export const BoardText = styled.Text`
+`;
+
+export const BoardGraph = styled.View`
+	width: 50px;
+	height: 50px;
+	border-radius: 50px;
+	border-width: 4px;
+	border-color: #dbdbdb;
+	border-top-color: #009846;
+	padding: 20px;
+`;
+
+export const BoardHeaderText = styled.Text`
+	margin-bottom: 12px;
+	font-size: 14px;
+	color: #1351b4;
+`;
+
+export const BoardFooterText = styled.Text`
+	text-align: center;
+	margin-top: 32px;
+	font-size: 14px;
+	color: #1351b4;
+`;
+
+export const RegisterDemand = styled.TouchableOpacity`
+	width: 100%;
+	border-width: 1px;
+	border-color: #1351b4;
+	background-color: #EAD555;
+	border-radius: 8px;
+	padding: 24px 12px;
+	justify-content: center;
+	align-items: center;
+	margin-top: 20px;
+`;
+
+export const RegisterText = styled.Text`
+	font-size: 20px;
+	font-weight: bold;
+	color: #1351b4;
+`;
+
+export const MainDisplayButtons = styled.View`
+	margin-top: 20px;
+	width: 100%;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const MainButtom = styled.TouchableOpacity`
+	width: 30%;
+	border-width: 1px;
+	border-color: #1351b4;
+	background-color: #DCE2EB;
+	border-radius: 8px;
+	padding: 24px 12px;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const ButtomText = styled.Text`
+	font-size: 12px;
+	font-weight: bold;
+	color: #1351b4;
+`;
