6c51a3008d0d3be6ae3da6364492eb7bd0046e0d
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-11 09:38:02 -0300

    feat: embrulhar toda aplicação com provedor de autenticação da Context API

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Routes from './src/pages/routes';
 import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/context/auth';
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Routes />
-		</NavigationContainer>
+		<AuthProvider>
+			<NavigationContainer>
+				<Routes />
+			</NavigationContainer>
+		</AuthProvider>
 	);
 }
