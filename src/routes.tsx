128263ff52320186c207738b3378e9234bcf5da1
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-11 00:01:27 -0300

    feat: adicionar boilerplate de navegação em pilha

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
 
-export default function routes() {
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+export default function Routes() {
+	<Stack.Navigator>
+		<Stack.Screen name='Home' component={Home} />
+	</Stack.Navigator>;
 	return <Home />;
 }
