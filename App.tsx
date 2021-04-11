8bcde648437da2de5b069611547db0d0db11a566
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-10 23:56:01 -0300

    feat: embrulhar todas as rotas no container de navegação

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Routes from './src/routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-	return <Routes />;
+	return (
+		<NavigationContainer>
+			<Routes />
+		</NavigationContainer>
+	);
 }
