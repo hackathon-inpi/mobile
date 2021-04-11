acab1dfeaa734443849ff0114959728c48095dbd
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-04-11 00:15:20 -0300

    feat: adicionar ícones na barra inferior de navegação em abas

--- src/pages/Tabs/index.tsx
+++ src/pages/Tabs/index.tsx
@@ -2,13 +2,32 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../Home';
 
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name='Home' component={Home} />
-			<Tab.Screen name='Settings' component={Home} />
+			<Tab.Screen
+				name='Home'
+				component={Home}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Entypo name='home' size={24} color='black' />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Perfil'
+				component={Home}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<FontAwesome5 name='user-alt' size={24} color='black' />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	);
 }
